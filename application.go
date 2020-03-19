d8b2773f20f54d83f6eafe4de154e07a52afed1b
Matthew Chartier <MChartier@gmail.com>
2020-03-19 11:20:58 -0700

    Integrating with Amazon RDS database (#2)
    Adding database.go file defining a function that establishes a connection with a remote database
    given an endpoint and password defined through environment variables.
    
    Updating application.go to include a handler on the "/counter" route that serves up the latest values
    of an integer counter and then increments the value in the database.
    
    We are using password authentication to connect to the database instance because IAM authentication
    is not available for the small instance size that we have provisioned in AWS. IAM authentication will also
    not be supported if and when we switch to using a serverless deployment of Aurora, so we may need to continue
    connecting in this way. Environment variables are used to store the endpoint and password so that we can configure
    different environments to connect to different instances of the database and to ensure that we do not check in
    database credentials to source control.

---
+++ application.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"database/sql"
+)
+
+// Defines port to listen on for requests
+const port = ":5000"
+
+// Responds to an HTTP request with a friendly response message
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!")
+}
+
+// Responds to an HTTP request by displaying the latest counter value and incrementing it in the database
+func counterHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	// Defensive check of DB instance passed to function
+	if db == nil {
+		fmt.Fprintf(w, "Db is nil :(")
+		return
+	}
+
+	// Query the database for the current counter value
+	readRows, readRrr := db.Query("SELECT Value from Counters WHERE ID='1';")
+	if readRrr != nil {
+		panic("Failed to query database. Error: " + readRrr.Error())
+	}
+	defer readRows.Close()
+
+	// Read value from row response
+	var value int
+	readRows.Next()
+	readRows.Scan(&value)
+
+	// Query the database to update the counter value
+	updateRows, updateErr := db.Query(fmt.Sprintf("UPDATE Counters SET Value='%d' WHERE ID='%d'", value+1, 1))
+	if updateErr != nil {
+		panic("Failed to update counter value. Error: " + updateErr.Error())
+	}
+	defer updateRows.Close()
+
+	// Return response
+	fmt.Fprintf(w, "Counter value is %d", value)
+}
+
+// Main entry point function for MatterhornApiService
+func main() {
+	run()
+	os.Exit(1)
+}
+
+func run() {
+	db, err := DbConnect()
+	if err != nil {
+		panic("Failed to connect to DB! Error: " + err.Error())
+	}
+
+	if db == nil {
+		panic("DB is nil immediately after assignment!")
+	}
+
+	defer db.Close()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { helloWorld(w, r) })
+	http.HandleFunc("/counter", func(w http.ResponseWriter, r *http.Request) { counterHandler(w, r, db) })
+	http.ListenAndServe(port, nil)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Matterhorn-Apps/MatterhornApiService
 
 go 1.14
+
+require (
+	github.com/aws/aws-sdk-go v1.29.27
+	github.com/go-sql-driver/mysql v1.5.0
+)
