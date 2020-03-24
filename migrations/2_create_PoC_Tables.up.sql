4caf3797d539c37273c5405abf883ca16fe79b1a
Matthew Chartier <mchartier@outlook.com>
2020-03-23 20:14:04 -0700

    Updating database migrations to better fit PoC API design

--- migrations/2_create_PoC_Tables.up.sql
+++ migrations/2_create_PoC_Tables.up.sql
@@ -1,32 +1,37 @@
 CREATE TABLE IF NOT EXISTS Users (
   UserID int,
-  DisplayName VARCHAR(50) NOT NULL DEFAULT 'Anonymous',
+  DisplayName VARCHAR(50),
+  Age int,
+  Height int,
+  Sex ENUM('Female', 'Male'),
+  Weight int,
   PRIMARY KEY (UserID)
+);
+
+CREATE TABLE IF NOT EXISTS CalorieGoals (
+    ID int AUTO_INCREMENT,
+    UserID int NOT NULL,
+    Calories int NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
 CREATE TABLE IF NOT EXISTS ExerciseRecords (
     ID int AUTO_INCREMENT,
-    UserID int,
-    Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UserID int NOT NULL,
     Calories int NOT NULL,
+    Label VARCHAR(255) DEFAULT '',
+    Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (ID),
     FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
 CREATE TABLE IF NOT EXISTS FoodRecords (
     ID int AUTO_INCREMENT,
-    UserID int,
-    Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UserID int NOT NULL,
     Calories int NOT NULL,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (UserID) REFERENCES Users(UserID)
-);
-
-CREATE TABLE IF NOT EXISTS WeightRecords (
-    ID int AUTO_INCREMENT,
-    UserID int,
+    Label VARCHAR(255) DEFAULT '',
     Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    Weight Decimal(6,2) NOT NULL,
     PRIMARY KEY (ID),
     FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
